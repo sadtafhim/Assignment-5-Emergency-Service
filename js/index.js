35a36c85f54ab17a05db0debb724df23712b6514
Saad <tafhim.sad@gmail.com>
2025-08-29 14:44:41 +0600

    seventh commit

--- js/index.js
+++ js/index.js
@@ -15,9 +15,11 @@ document.getElementById("police-heart").addEventListener("click", function () {
 document.getElementById("fire-heart").addEventListener("click", function () {
   heartCountIncrease();
 });
-document.getElementById("ambulance-heart").addEventListener("click", function () {
-  heartCountIncrease();
-});
+document
+  .getElementById("ambulance-heart")
+  .addEventListener("click", function () {
+    heartCountIncrease();
+  });
 document.getElementById("women-heart").addEventListener("click", function () {
   heartCountIncrease();
 });
@@ -32,4 +34,64 @@ document.getElementById("rail-heart").addEventListener("click", function () {
 });
 document.getElementById("brac-heart").addEventListener("click", function () {
   heartCountIncrease();
+});
+
+// number alert
+
+function alertAndAdd(nameid, numberid) {
+  const name = document.getElementById(nameid).innerText;
+  const number = document.getElementById(numberid).innerText;
+
+  alert("Calling " + name + " " + number);
+
+  const div = document.createElement("div");
+  div.classList.add("flex", "justify-between", "items-center", "gap-2", "mb-1");
+
+  const now = new Date().toLocaleTimeString();
+
+  div.innerHTML = `
+    <div>
+      <h1 class="hind-madurai-regular font-bold">${name}</h1>
+      <p class="hind-madurai-regular">${number}</p>
+    </div>
+    <h1 class="hind-madurai-regular font-bold">${now}</h1>
+  `;
+
+  document.getElementById("call-log").appendChild(div);
+}
+
+document.getElementById("nat-em-call").addEventListener("click", function () {
+  alertAndAdd("nat-em-name", "nat-em-number");
+});
+document.getElementById("police-call").addEventListener("click", function () {
+  alertAndAdd("police-name", "police-number");
+});
+document.getElementById("fire-call").addEventListener("click", function () {
+  alertAndAdd("fire-name", "fire-number");
+});
+document
+  .getElementById("ambulance-call")
+  .addEventListener("click", function () {
+    alertAndAdd("ambulance-name", "ambulance-number");
+  });
+document.getElementById("women-call").addEventListener("click", function () {
+  alertAndAdd("women-name", "women-number");
+});
+document.getElementById("anti-call").addEventListener("click", function () {
+  alertAndAdd("anti-name", "anti-number");
+});
+document.getElementById("electro-call").addEventListener("click", function () {
+  alertAndAdd("electro-name", "electro-number");
+});
+document.getElementById("brac-call").addEventListener("click", function () {
+  alertAndAdd("brac-name", "brac-number");
+});
+document.getElementById("rail-call").addEventListener("click", function () {
+  alertAndAdd("rail-name", "rail-number");
+});
+
+// clearing the window
+
+document.getElementById("clear").addEventListener("click", function () {
+  document.getElementById("call-log").innerHTML = "";
 });
